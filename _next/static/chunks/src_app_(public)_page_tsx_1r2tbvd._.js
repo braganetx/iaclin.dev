ea2ef8578f562dbe2538aef3fdf522ea4c16ac31
Adão Braga <braganet@live.com>
2026-06-16 21:42:42 -0300

    feat: updated landing page from Next.js export

---
+++ _next/static/chunks/src_app_(public)_page_tsx_1r2tbvd._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/src_app_(public)_page_tsx_05gtph8._.js","static/chunks/node_modules_1gl4507._.js"],
+    source: "dynamic"
+});
